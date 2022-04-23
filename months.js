c4e26716a279d2f2b60bf80ba6b540953135ecb1
Prajna1999 <gituprajna20@gmail.com>
2022-04-23 19:52:10 +0530

    feat: local storage functionality

---
+++ months.js
@@ -0,0 +1,7 @@
+// this file contains an array of months.
+// it's just to demonstrate the exportability.
+
+const months=["January", "February", "March", "April", "May", "June", "July",
+"August", "September", "October", "November", "December"];
+
+export {months};
--- script.js
+++ script.js
@@ -1,1 +1,65 @@
-document.querySelector("textarea").value=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, est alias culpa tenetur laborum minima facere consequuntur recusandae atque. Dolorum est exercitationem facere voluptatem, dignissimos similique! Tenetur atque sequi ut."
+
+import  {months} from "./months.js";
+
+// grab the elements of the dom.
+const addBox=document.querySelector(".add-box");
+const popupBox=document.querySelector(".popup-box");
+const cancelBtn=document.querySelector(".content header i");
+const addBtn=document.querySelector("#addBtn");
+const title=document.querySelector("form input");
+const desc=document.querySelector("#textarea");
+
+// add event listner for the popup.
+addBox.addEventListener("click", (e)=>{
+    // console.log(e.target);
+    popupBox.classList.add("show");
+})
+
+// add event listener to the cancel btn.
+cancelBtn.addEventListener("click", ()=>{
+    popupBox.classList.remove("show");
+});
+
+// add event listener to the add note button.
+// here the noteinfo will be stored in the localstorage as a notes object
+addBtn.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const titleInfo=title.value.trim();
+    const descInfo=desc.value.trim();
+    
+    if(titleInfo || descInfo){
+
+        const dateObj=new Date();
+    
+        const dateInfo={
+            month:dateObj.getMonth(),
+            day:dateObj.getMonth(),
+            year:dateObj.getFullYear()
+        }
+      
+        // saving to the local storage.
+        let notes=[];
+
+        if(!notes){
+            notes=[];
+        }else{
+            const noteInfo={
+                title:titleInfo,
+                desc:descInfo,
+                dateInfo:dateInfo
+        
+            };
+            notes.push(noteInfo);
+            localStorage.setItem("notearray",JSON.stringify(notes));
+    
+        }
+      
+    }
+
+
+
+    
+
+
+    
+})
